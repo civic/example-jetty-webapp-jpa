9a5f477a174df2bf3da8c7a0901c9bb4365b2350
civic <tsskmail@gmail.com>
2014-07-22 07:50:39 +0900

    temp

--- src/main/java/jetty_webapp/rest/SampleJaxRs.java
+++ src/main/java/jetty_webapp/rest/SampleJaxRs.java
@@ -2,9 +2,11 @@
 package jetty_webapp.rest;
 
 import javax.json.Json;
+import javax.json.JsonObject;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 
 /**
  * REST Web Service
@@ -13,9 +15,9 @@ import javax.ws.rs.Path;
 public class SampleJaxRs {
 
     @GET
-    @Produces("text/plain")
-    public String getText() {
-        return "Hello JAX-RS";
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getText() {
+        return Json.createObjectBuilder().add("foo", 100).build();
     }
 
 }
