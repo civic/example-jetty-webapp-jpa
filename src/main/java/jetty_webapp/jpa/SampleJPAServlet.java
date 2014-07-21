95bec4e5c9020343d47a79cf5612e4e1e89ecff3
civic <tsskmail@gmail.com>
2014-07-22 00:15:48 +0900

    glassfish web-profile

--- src/main/java/jetty_webapp/jpa/SampleJPAServlet.java
+++ src/main/java/jetty_webapp/jpa/SampleJPAServlet.java
@@ -5,6 +5,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,12 +19,13 @@ import jetty_webapp.jpa.entity.Customer;
  */
 @WebServlet("/jpa-servlet")
 public class SampleJPAServlet extends HttpServlet {
+    @PersistenceUnit(unitName = "webapp-jpa-pu")
     private EntityManagerFactory emf;
 
     @Override
     public void init() throws ServletException {
         //アノテーションからは取れないのでコードで取得
-        emf = Persistence.createEntityManagerFactory("webapp-jpa-pu");
+        //emf = Persistence.createEntityManagerFactory("webapp-jpa-pu");
     }
 
     @Override
@@ -35,10 +38,8 @@ public class SampleJPAServlet extends HttpServlet {
             Customer customer = new Customer();
             customer.setCustomerName("c-"+ System.currentTimeMillis());
 
-            EntityTransaction tx = em.getTransaction();
-            tx.begin();
             em.persist(customer);
-            tx.commit();
+
 
             //Customerの検索
             for (Customer c : em.createNamedQuery("Customer.findAll", Customer.class).getResultList()){
